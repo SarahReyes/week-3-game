15574c4449470342324a2dfb4d7ff7436dc8178e
SarahReyes <siwreyes@gmail.com>
2016-08-10 21:14:52 -0500

    working on word guess function in JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,59 @@
 
 
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+//         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+//         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// document.onkeyup = function(event) {
-//     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
 
 //     console.log(userGuessBeforeConversion);
 
 // }
-var tvShow = "Friends";
-var numberOfGuesses = 15;
-var lettersPicked;
-var wins = 0;
+var tvShows          = ["friends", "seinfeld", "roseanne", "ducktales"];
+var remainingGuesses = 15;
+var lettersPicked    = [];
+var wins             = 0;
+var tvShow           = tvShows[Math.floor(Math.random() * tvShows.length)];
 
-document.onkeyup = function(event) {
-   var html = "<div class='top-line'><h3>Guess A Letter To Name That TV Show</h3></div>" +
-              "<div class='current-tv-show'>" + tvShow + "</div>" +
-              "<div class='number-of-guesses'><h3>Number Of Guesses Remaining: " + numberOfGuesses + "</h3></div>" +
-              "<div class='letters-picked'><h3>Letters Picked: " + lettersPicked + "</h3></div>" +
-              "<div class='wins'><h3>Wins: " + wins + "</h3></div>";
-
-   document.querySelector('#tv-game').innerHTML = html;
-
+var buildDisplayString = function() {
+  var displayString = "";
+  // loop thru tvShow string
+  for (var i = 0; i < tvShow.length; i++) {
+    // decide whether to use actual character or "_" and add that to our new string
+    // the user guesses a letter
+    if (false) {
+      // we put that letter into the string
+      displayString = displayString + tvShow.charAt(i);
+    }
+    else {
+      //we put an underscore into the string
+      displayString = displayString + "_";
+    }
+  }
 }
 
+var getUserChoice = function() {
+  $('body').keypress(function(event) {
+    return String.fromCharCode(event.keyCode).toLowerCase();
+  });
+}
+
+$(document).ready(function() {
+  $("#tv").on("click", function() {
+     var html = "<div class='top-line'><h3>Guess A Letter To Name That TV Show</h3></div>" +
+                "<div class='current-tv-show'>" + tvShow + "</div>" +
+                "<div class='number-of-guesses'><h3>Number Of Guesses Remaining: " + remainingGuesses + "</h3></div>" +
+                "<div class='letters-picked'><h3>Letters Picked: " + lettersPicked + "</h3></div>" +
+                "<div class='wins'><h3>Wins: " + wins + "</h3></div>";
+
+     document.querySelector('#tv-game').innerHTML = html;
+
+     var userGuess = getUserChoice();
+
+     console.log(userGuess);
+  })
+})
 
 
-//I need to make the "start" text go away when someone presses any key
 
-    // possibly use for replacing the _? $("a").replaceWith( "o" );
+

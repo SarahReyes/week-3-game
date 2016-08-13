9d1426ab475f333412af0969234b4b21b654df37
SarahReyes <siwreyes@gmail.com>
2016-08-12 20:53:12 -0500

    created lots of fuctions, not done yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,55 +4,99 @@
 //         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 //         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+var pickRandom = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
 
-
-//     console.log(userGuessBeforeConversion);
-
-// }
-var tvShows          = ["friends", "seinfeld", "roseanne", "ducktales"];
+var tvShows = ["friends", "seinfeld", "roseanne", "ducktales"];
 var remainingGuesses = 15;
-var lettersPicked    = [];
-var wins             = 0;
-var tvShow           = tvShows[Math.floor(Math.random() * tvShows.length)];
+var lettersPicked = [];
+var wins = 0;
+var tvShow = pickRandom(tvShows);
+var userGuess;
 
 var buildDisplayString = function() {
   var displayString = "";
   // loop thru tvShow string
   for (var i = 0; i < tvShow.length; i++) {
     // decide whether to use actual character or "_" and add that to our new string
-    // the user guesses a letter
-    if (false) {
+    if (hasUserPickedThisLetter(tvShow[i])) {
       // we put that letter into the string
-      displayString = displayString + tvShow.charAt(i);
+      displayString = displayString + tvShow[i];
     }
     else {
       //we put an underscore into the string
       displayString = displayString + "_";
     }
   }
+  return displayString
 }
 
-var getUserChoice = function() {
-  $('body').keypress(function(event) {
-    return String.fromCharCode(event.keyCode).toLowerCase();
-  });
-}
+//function to see if the letter the user picks is inside of the word
+var hasUserPickedThisLetter = function(letter) {
+  var answer = false;
+  for (var i = 0; i < lettersPicked.length; i++) {
+    if (lettersPicked[i] === letter) {
+      answer = true;
+    }
+  }
+  return answer;
+};
+
+var updateHtml = function(){
+  document.querySelector('#tv-game').innerHTML = "<div class='top-line'><h3>Guess A Letter To Name That TV Show</h3></div>" +
+    "<div class='current-tv-show'>" + buildDisplayString() + "</div>" +
+    "<div class='number-of-guesses'><h3>Number Of Guesses Remaining: " + remainingGuesses + "</h3></div>" +
+    "<div class='letters-picked'><h3>Letters Picked: " + lettersPicked + "</h3></div>" +
+    "<div class='wins'><h3>Wins: " + wins + "</h3></div>";
+};
+
+var noUnderscores = function(){
+  var answer = true;
+  for (var i = 0; i < tvShow.length; i++) {
+    if (buildDisplayString()[i] === "_") {
+      answer = false;
+    }
+  }
+  return answer;
+};
 
 $(document).ready(function() {
   $("#tv").on("click", function() {
-     var html = "<div class='top-line'><h3>Guess A Letter To Name That TV Show</h3></div>" +
-                "<div class='current-tv-show'>" + tvShow + "</div>" +
-                "<div class='number-of-guesses'><h3>Number Of Guesses Remaining: " + remainingGuesses + "</h3></div>" +
-                "<div class='letters-picked'><h3>Letters Picked: " + lettersPicked + "</h3></div>" +
-                "<div class='wins'><h3>Wins: " + wins + "</h3></div>";
+    updateHtml();
+    //user guess (key on keyboard pressed) is logged. Using the function it's called up in the HTMLs
+    document.onkeyup = function(event) {
+      userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+      //has the user already picked the letter, don't repeat in the line
+      if (hasUserPickedThisLetter(userGuess)) {
+        lettersPicked.push(userGuess);
+      }
+      //deducted from remaining guesses each time there is a user guess
+      remainingGuesses--;
 
-     document.querySelector('#tv-game').innerHTML = html;
+      if (noUnderscores()) {
+        wins = wins + 1;
+        }
 
-     var userGuess = getUserChoice();
+      updateHtml();
+    };
+  });
+});
 
-     console.log(userGuess);
-  })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
